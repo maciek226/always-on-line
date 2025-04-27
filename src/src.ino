4943fa692e12df6b154019fa49fe3b36598050cb
maciej <maciej@procero.ca>
2025-04-27 12:54:22 -0400

    Pings work all around!

--- src/src.ino
+++ src/src.ino
@@ -3,7 +3,6 @@
 #include <ESPping.h>
 #include "secrets.h"
 
-
 const char* pingAddress = "192.168.50.100";
 
 IPAddress local_IP(192, 168, 50, 99);
@@ -13,7 +12,7 @@ IPAddress subnet(255, 255, 0, 0);
 WiFiClient client;
 
 const long interval = 10000;  // 10 seconds
-unsigned long previousMillis = 0;
+
 
 void check_wifi_shield() {
   if (WiFi.status() == WL_NO_SHIELD) {
@@ -54,6 +53,42 @@ void print_mac() {
                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
 
+bool check_gateway(){
+  IPAddress gateway(192,168,50,1);
+  if (Ping.ping(gateway)){
+    Serial.println("Gateway responded");
+    return true;
+  }
+  else{
+    Serial.println("Gatway did not respond");
+    return false;
+  }
+}
+
+bool check_xopalt(){
+  IPAddress xopalt(192,168,50,100);
+  if (Ping.ping(xopalt)){
+    Serial.println("Xopalt responded");
+    return true;
+  }
+  else{
+    Serial.println("Xopalt did not respond");
+    return false;
+  }
+}
+
+bool check_google(){
+  IPAddress google(8,8,8,8);
+  if (Ping.ping(google)){
+    Serial.println("Google responded");
+    return true;
+  }
+  else{
+    Serial.println("Google did not respond");
+    return false;
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
@@ -81,34 +116,83 @@ void setup() {
   
 }
 
-bool check_google() {
-  if (client.connect("www.neverssl.com", 80)) {
-    Serial.println("Successfully connected to nastrurcja!");
-    client.stop();
-    return true;
-  } else {
-    Serial.println("Failed to connect to nasturcja.");
-    return false;
-  }
-}
+unsigned long previous_milli_gateway = 0;
+unsigned long previous_milli_google = 0;
+unsigned long previous_milli_xopalt = 0;
 
-// bool check_gateway() {
-//   if (client.connect("192.168.50.1", 80)) {
-//     Serial.println("Successfully connected to Gateway!");
-//     client.stop();
-//     return true;
-//   } else {
-//     Serial.println("Failed to connect to Gateway.");
-//     return false;
-//   }
-// }
+const long interval_gateway_max = 600'000;
+const long interval_google_max = 600'000;
+const long interval_xopalt_max = 600'000;
+
+const long interval_gateway_min = 1'000;
+const long interval_google_min = 1'000;
+const long interval_xopalt_min = 1'000;
+
+const long interval_gateway_deafult = 10'000;
+const long interval_google_deafult = 10'000;
+const long interval_xopalt_deafult = 10'000;
+
+long interval_gateway = 10'000;
+long interval_google = 10'000;
+long interval_xopalt = 10'000;
+
+int gateway_fails = 0;
+int google_fails = 0;
+int xopalt_fails = 0;
+
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    // check_gateway();
-    check_google();
+  if (currentMillis - previous_milli_google >= interval_google) {
+    previous_milli_google = currentMillis;
+    if (!check_google()){
+      google_fails++;
+      interval_google = interval_google_min;
+    }
+    else{
+      google_fails = 0;
+      interval_google = interval_google_deafult;
+    }
+    if (google_fails > 5){
+      Serial.println("Google failed too many times - restarting the modem");
+      interval_google = interval_google_max;
+    }
+  }
+
+  if (currentMillis - previous_milli_xopalt >= interval_xopalt) {
+    previous_milli_xopalt = currentMillis;
+    if (!check_xopalt()){
+      xopalt_fails++;
+      interval_xopalt = interval_xopalt_min;
+    }
+    else{
+      xopalt_fails = 0;
+      interval_xopalt = interval_xopalt_deafult;
+    }
+    if (xopalt_fails > 5){
+      Serial.println("Xopalt failed too many times - sending a wake signal");
+      interval_xopalt = interval_xopalt_max;
+    }
+
+  }
+
+  if (currentMillis - previous_milli_gateway >= interval_gateway) {
+    previous_milli_gateway = currentMillis;
+
+    if (!check_gateway()){
+      gateway_fails++;
+      interval_gateway = interval_gateway_min;
+    }
+    else{
+      gateway_fails = 0;
+      interval_gateway = interval_gateway_deafult;
+    }
+    if (gateway_fails > 5){
+      Serial.println("Gateway failed too many times - restarting");
+      interval_gateway = interval_gateway_max;
+    }
+
+
   }
 }
